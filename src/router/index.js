94fc1dedb376a4b0437d9259f03f9da87f77dd90
Paul <frankypie202@gmail.com>
2020-10-27 14:30:32 +0100

    Privacy(WP)
    Pirivacy(WP)

--- src/router/index.js
+++ src/router/index.js
@@ -101,6 +101,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Highscore.vue')
+  },
+  {
+    path: '/impressum',
+    name: 'Impressum',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Impressum.vue')
+  },
+  {
+    path: '/datenschutz',
+    name: 'Datenschutz',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Datenschutz.vue')
   }
 ]
 
