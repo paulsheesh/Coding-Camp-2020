fade3b4379bc0cada74a1ec79eac36572970cc68
Paul <frankypie202@gmail.com>
2020-10-27 14:36:21 +0100

    Revert "Revert "Privacy(WP)""
    This reverts commit b398949d504b288630ef10e4b46498ce4f4d1ff8.

---
+++ src/views/Impressum.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Impressum</h1>
+        <h2>Diensteanbieter</h2>
+        <p>Fabienn Schwarz-Loy</p>
+        <p>Radolfzeller str. 2</p>
+        <p>78345 Moos</p>
+        <p>Deutschland</p>
+        <h2>Kontaktmöglichkeiten</h2><p><strong>E-Mail-Adresse:</strong> <a href="mailto:seieinheld@heldsein.com">seieinheld@heldsein.com</a></p>
+    </div>
+</template>
